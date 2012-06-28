cf9254a3e4675eda1093cddb5c26619ca8c6eb12
Brian O'Neill <bone@alumni.brown.edu>
2012-06-28 09:28:21 -0400

    Start of Spring Data JPA

---
+++ README.md
@@ -0,0 +1,1 @@
+Spring Data JPA layer for Cassandra.
---
+++ src/main/java/org/springframework/data/cassandra/CassandraRepository.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2010-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.mongodb.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * Mongo specific {@link org.springframework.data.repository.Repository} interface.
+ * 
+ * @author Oliver Gierke
+ */
+@NoRepositoryBean
+public interface MongoRepository<T, ID extends Serializable> extends PagingAndSortingRepository<T, ID> {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.CrudRepository#save(java.lang.Iterable)
+	 */
+	<S extends T> List<S> save(Iterable<S> entites);
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.CrudRepository#findAll()
+	 */
+	List<T> findAll();
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.PagingAndSortingRepository#findAll(org.springframework.data.domain.Sort)
+	 */
+	List<T> findAll(Sort sort);
+}
