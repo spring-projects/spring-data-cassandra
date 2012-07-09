e1e5a8c1eda368c283c0b0ec49802606fccd52e3
Brian O'Neill <bone@alumni.brown.edu>
2012-07-09 16:18:43 -0400

    Working Kundera example.

---
+++ src/test/java/org/springframework/data/cassandra/repository/UserTest.java
@@ -0,0 +1,27 @@
+package org.springframework.data.cassandra.repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.Test;
+
+public class UserTest {
+    
+    @Test
+    public void testWrite() {
+        User user = new User();
+        user.setUserId("0001");
+        user.setFirstName("John");
+        user.setLastName("Smith");
+        user.setCity("London");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("cassandra_pu");
+        EntityManager em = emf.createEntityManager();
+
+        em.persist(user);
+        em.close();
+        emf.close();
+    }
+    
+}
