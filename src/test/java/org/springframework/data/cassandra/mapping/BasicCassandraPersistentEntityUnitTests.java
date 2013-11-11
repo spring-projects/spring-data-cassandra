0480ee3ea6517cbc314a8ac8767961359a0277e7
dwebb <dwebb@brightmove.com>
2013-11-11 14:22:51 -0500

    Added Embedded cassandra-unit to existing tests and all are passing.

--- src/test/java/org/springframework/data/cassandra/mapping/BasicCassandraPersistentEntityUnitTests.java
+++ src/test/java/org/springframework/data/cassandra/mapping/BasicCassandraPersistentEntityUnitTests.java
@@ -19,6 +19,14 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
+
+import org.apache.cassandra.exceptions.ConfigurationException;
+import org.apache.thrift.transport.TTransportException;
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -38,6 +46,12 @@ public class BasicCassandraPersistentEntityUnitTests {
 
 	@Mock
 	ApplicationContext context;
+	
+    @BeforeClass
+    public static void startCassandra()
+            throws IOException, TTransportException, ConfigurationException, InterruptedException {
+        EmbeddedCassandraServerHelper.startEmbeddedCassandra("cassandra.yaml");
+    }
 
 	@Test
 	public void subclassInheritsAtDocumentAnnotation() {
@@ -70,6 +84,16 @@ public class BasicCassandraPersistentEntityUnitTests {
 
 		assertThat(entity.getTable(), is("user_line"));
 	}
+	
+    @After
+    public void clearCassandra() {
+        EmbeddedCassandraServerHelper.cleanEmbeddedCassandra();
+    }
+
+    @AfterClass
+    public static void stopCassandra() {
+    	EmbeddedCassandraServerHelper.stopEmbeddedCassandra();
+    }
 
 	@Table(name = "messages")
 	class Message {
