c42cec6f111435ffdfeb82201771ec390f7327a5
Matthew Adams <matthew@matthewadams.me>
2013-11-14 10:02:54 -0600

    CQLBuilder -> CqlBuilder

--- src/main/java/org/springframework/data/cassandra/cql/CqlBuilder.java
+++ src/main/java/org/springframework/data/cassandra/cql/CqlBuilder.java
@@ -1,6 +1,6 @@
 package org.springframework.data.cassandra.cql;
 
-public abstract class CQLBuilder {
+public abstract class CqlBuilder {
 
 	public static CreateTable createTable(String tableName) {
 		return null;
