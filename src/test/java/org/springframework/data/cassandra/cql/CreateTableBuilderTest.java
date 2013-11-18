6c3b2c2a8af891642e5aef300c1db695f346edcc
Matthew Adams <matthew@matthewadams.me>
2013-11-18 17:48:30 -0600

    added TableOption enums, MapBuilder

--- src/test/java/org/springframework/data/cassandra/cql/CreateTableBuilderTest.java
+++ src/test/java/org/springframework/data/cassandra/cql/CreateTableBuilderTest.java
@@ -1,9 +1,11 @@
 package org.springframework.data.cassandra.cql;
 
 import static org.springframework.data.cassandra.cql.builder.CqlBuilder.createTable;
-
-import java.util.HashMap;
-import java.util.Map;
+import static org.springframework.data.cassandra.cql.builder.MapBuilder.map;
+import static org.springframework.data.cassandra.cql.builder.TableOption.BLOOM_FILTER_FP_CHANCE;
+import static org.springframework.data.cassandra.cql.builder.TableOption.COMMENT;
+import static org.springframework.data.cassandra.cql.builder.TableOption.COMPACTION;
+import static org.springframework.data.cassandra.cql.builder.TableOption.CompactionOption.TOMBSTONE_THRESHOLD;
 
 import org.junit.Test;
 import org.springframework.data.cassandra.cql.builder.CreateTableBuilder;
@@ -16,25 +18,20 @@ public class CreateTableBuilderTest {
 	public void createTableTest() {
 		String name = "mytable";
 		DataType type0 = DataType.text();
-		String partition0 = "partitionKey0";
+		String partKey0 = "partitionKey0";
 		String partition1 = "partitionKey1";
 		String primary0 = "primary0";
 		DataType type1 = DataType.text();
 		String column1 = "column1";
 		DataType type2 = DataType.bigint();
 		String column2 = "column2";
-		Object value2 = "this is a comment";
-		String option2 = "comment";
-		Object value3 = "0.00075";
-		String option3 = "bloom_filter_fp_chance";
-		Map<String, Object> value4 = new HashMap<String, Object>();
-		value4.put("class", "LeveledCompactionStrategy");
-		String option4 = "compaction";
+		Object comment = "this is a comment";
+		Object bloom = "0.00075";
 
-		CreateTableBuilder builder = createTable().ifNotExists().name(name).partitionKeyColumn(partition0, type0)
+		CreateTableBuilder builder = createTable().ifNotExists().name(name).partitionKeyColumn(partKey0, type0)
 				.partitionKeyColumn(partition1, type0).primaryKeyColumn(primary0, type0).column(column1, type1)
-				.column(column2, type2).withQuoted(option2, value2).withUnquoted(option3, value3).with(option4, value4)
-				.withCompactStorage();
+				.column(column2, type2).with(COMMENT, comment).with(BLOOM_FILTER_FP_CHANCE, bloom)
+				.with(COMPACTION, map().entry(TOMBSTONE_THRESHOLD, "0.15"));
 
 		String cql = builder.toCql();
 		System.out.println(cql);
