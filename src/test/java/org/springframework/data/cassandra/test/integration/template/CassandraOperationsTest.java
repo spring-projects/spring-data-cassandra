58a0323408467361661eb53ca6756eeff2ce0a3b
David Webb <dwebb@prowaveconsulting.com>
2013-11-25 12:20:46 -0500

    DATACASS-39 : WIP : Removed failing test.

--- src/test/java/org/springframework/data/cassandra/test/integration/template/CassandraOperationsTest.java
+++ src/test/java/org/springframework/data/cassandra/test/integration/template/CassandraOperationsTest.java
@@ -190,11 +190,6 @@ public class CassandraOperationsTest {
 	}
 
 	@Test
-	public void failingTest() {
-		assertNotNull(null);
-	}
-
-	@Test
 	public void boundStatementFactoryTest() {
 
 		String cql = "insert into book (isbn, title, author, pages) values (?, ?, ?, ?)";
