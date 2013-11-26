86224eb06db8c56728c996d6466c7c29108e6afb
Matthew Adams <matthew@matthewadams.me>
2013-11-25 18:33:44 -0600

    spring-cassandra now compiling

--- src/main/java/org/springframework/cassandra/core/keyspace/DropColumnSpecification.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.cassandra.core.keyspace;
-
-public class DropColumnSpecification extends ColumnChangeSpecification {
-
-	public DropColumnSpecification(String name) {
-		super(name);
-	}
-}
--- src/main/java/org/springframework/data/cassandra/core/FieldNameToColumnNameConverter.java
+++
@@ -1,6 +0,0 @@
-package org.springframework.data.cassandra.core;
-
-import org.springframework.core.convert.converter.Converter;
-
-public interface FieldNameToColumnNameConverter extends Converter<String, String> {
-}
