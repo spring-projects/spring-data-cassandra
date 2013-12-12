3c04ddf00848254e411bc866711618e31f541a5d
Matthew Adams <matthew@matthewadams.me>
2013-12-12 09:45:26 -0600

    moved Keyspace class from s-c to s-d-c in prep for complete removal

--- spring-data-cassandra/src/main/java/org/springframework/data/cassandra/core/Keyspace.java
+++ spring-data-cassandra/src/main/java/org/springframework/data/cassandra/core/Keyspace.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.cassandra.core;
+package org.springframework.data.cassandra.core;
 
 import com.datastax.driver.core.Session;
 
