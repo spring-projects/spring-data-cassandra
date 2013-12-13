8d97077c3c484f89b99cb49625b164585c1f56a8
David Webb <dwebb@prowaveconsulting.com>
2013-12-13 16:42:11 -0500

    Merged DATACASS-62

---
+++ spring-cassandra/src/main/java/org/springframework/cassandra/core/keyspace/IndexDescriptor.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2011-2013 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cassandra.core.keyspace;
+
+
+/**
+ * Describes an index.
+ * 
+ * @author Matthew T. Adams
+ * @author David Webb
+ */
+public interface IndexDescriptor {
+
+	/**
+	 * Returns the name of the index.
+	 */
+	String getName();
+
+	/**
+	 * Returns the table name for the index
+	 */
+	String getTableName();
+
+	/**
+	 * Returns the name of the index as an identifer or quoted identifier as appropriate.
+	 */
+	String getNameAsIdentifier();
+
+	/**
+	 * Returns the name of the table as an identifer or quoted identifier as appropriate.
+	 */
+	String getTableNameAsIdentifier();
+
+	String getColumnName();
+
+	String getUsing();
+
+	boolean isCustom();
+
+}
