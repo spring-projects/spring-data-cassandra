369cf44e46ebf9a10de97770fc3688858d49531d
john-mcpeek <juan.mcpeek@gmail.com>
2013-12-14 13:16:31 -0500

    Added the CqlGenerators for Keyspace.

--- spring-cassandra/src/main/java/org/springframework/cassandra/core/keyspace/DropKeyspaceSpecification.java
+++ spring-cassandra/src/main/java/org/springframework/cassandra/core/keyspace/DropKeyspaceSpecification.java
@@ -2,4 +2,19 @@ package org.springframework.cassandra.core.keyspace;
 
 public class DropKeyspaceSpecification extends KeyspaceNameSpecification<DropKeyspaceSpecification> {
 
+	private boolean ifExists;
+
+	public DropKeyspaceSpecification ifExists() {
+		return ifExists(true);
+	}
+
+	public DropKeyspaceSpecification ifExists(boolean ifExists) {
+		this.ifExists = ifExists;
+		return this;
+	}
+
+	public boolean getIfExists() {
+		return ifExists;
+	}
+
 }
