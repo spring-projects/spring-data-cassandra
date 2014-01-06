05466b9e30ef6e09036858739aa07dc78a22013a
Matthew Adams <matthew@matthewadams.me>
2014-01-06 10:12:18 -0600

    fixed copy/paste error in comment; fixed generics issue in 'with(..)' builder methods

--- spring-cassandra/src/main/java/org/springframework/cassandra/core/keyspace/AlterKeyspaceSpecification.java
+++ spring-cassandra/src/main/java/org/springframework/cassandra/core/keyspace/AlterKeyspaceSpecification.java
@@ -3,7 +3,7 @@ package org.springframework.cassandra.core.keyspace;
 public class AlterKeyspaceSpecification extends KeyspaceOptionsSpecification<AlterKeyspaceSpecification> {
 
 	/**
-	 * Entry point into the {@link CreateKeyspaceSpecification}'s fluent API to create a keyspace. Convenient if imported
+	 * Entry point into the {@link AlterKeyspaceSpecification}'s fluent API to alter a keyspace. Convenient if imported
 	 * statically.
 	 */
 	public static AlterKeyspaceSpecification alterKeyspace() {
