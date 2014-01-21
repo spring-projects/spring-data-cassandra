bfcb48fb63b4ef85bbae7013b47d157662f4f134
David Webb <dwebb@prowaveconsulting.com>
2014-01-21 15:56:43 -0500

    Merged DATACASS-71 to master.

--- spring-cassandra/src/test/java/org/springframework/cassandra/test/integration/config/xml/PropertyPlaceholderNamespaceCreatingXmlConfigTest.java
+++ spring-cassandra/src/test/java/org/springframework/cassandra/test/integration/config/xml/PropertyPlaceholderNamespaceCreatingXmlConfigTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cassandra.core.CassandraOperations;
-import org.springframework.cassandra.test.integration.AbstractEmbeddedCassandraIntegrationTest;
+import org.springframework.cassandra.test.integration.AbstractKeyspaceCreatingIntegrationTest;
 import org.springframework.cassandra.test.integration.config.IntegrationTestUtils;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -18,14 +18,9 @@ import com.datastax.driver.core.Session;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
-public class PropertyPlaceholderNamespaceCreatingXmlConfigTest extends AbstractEmbeddedCassandraIntegrationTest {
+public class PropertyPlaceholderNamespaceCreatingXmlConfigTest extends AbstractKeyspaceCreatingIntegrationTest {
 
 	private static Logger log = LoggerFactory.getLogger(PropertyPlaceholderNamespaceCreatingXmlConfigTest.class);
-
-	@Override
-	protected String keyspace() {
-		return "ppncxcx";
-	}
 
 	@Inject
 	Session s;
@@ -36,8 +31,10 @@ public class PropertyPlaceholderNamespaceCreatingXmlConfigTest extends AbstractE
 	@Test
 	public void test() {
 		IntegrationTestUtils.assertSession(s);
-		IntegrationTestUtils.assertKeyspaceExists(keyspace(), s);
+
+		IntegrationTestUtils.assertKeyspaceExists("ppncxct", s);
 
 		assertNotNull(ops);
 	}
+
 }
