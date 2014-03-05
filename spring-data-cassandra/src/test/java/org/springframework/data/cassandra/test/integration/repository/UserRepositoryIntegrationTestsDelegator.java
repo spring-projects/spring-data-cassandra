633eb08ad87768549b013edee7cfbcbc00e070ce
Matthew Adams <matthew@matthewadams.me>
2014-03-05 12:41:20 -0600

    DATACASS-107 - fixed broken tests, abstracted UserRepositoryIntegrationTestsDelegator, added TODO class for derived query method tests

--- spring-data-cassandra/src/test/java/org/springframework/data/cassandra/test/integration/repository/UserRepositoryIntegrationTestsDelegator.java
+++ spring-data-cassandra/src/test/java/org/springframework/data/cassandra/test/integration/repository/UserRepositoryIntegrationTestsDelegator.java
@@ -29,7 +29,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @author Matthew T. Adams
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-public class UserRepositoryIntegrationTestsDelegator extends AbstractSpringDataEmbeddedCassandraIntegrationTest {
+public abstract class UserRepositoryIntegrationTestsDelegator extends
+		AbstractSpringDataEmbeddedCassandraIntegrationTest {
 
 	@Autowired
 	protected UserRepository repository;
