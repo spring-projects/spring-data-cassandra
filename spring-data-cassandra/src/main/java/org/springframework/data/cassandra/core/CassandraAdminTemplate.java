71a1ba5255b7c61bc882783b3229b2b249cdaa96
Mark Paluch <mpaluch@pivotal.io>
2016-04-28 14:12:30 +0200

    DATACASS-174 - Polishing.
    Guard getTableName if PersistentEntity is null. Add author tags. Update date in license headers. Remove unused code.
    
    Original pull request: #28.

--- spring-data-cassandra/src/main/java/org/springframework/data/cassandra/core/CassandraAdminTemplate.java
+++ spring-data-cassandra/src/main/java/org/springframework/data/cassandra/core/CassandraAdminTemplate.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2013-2014 the original author or authors
- * 
+ * Copyright 2013-2016 the original author or authors
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,6 +35,8 @@ import com.datastax.driver.core.TableMetadata;
 
 /**
  * Default implementation of {@link CassandraAdminOperations}.
+ *
+ * @author Mark Paluch
  */
 public class CassandraAdminTemplate extends CassandraTemplate implements CassandraAdminOperations {
 
@@ -42,7 +44,7 @@ public class CassandraAdminTemplate extends CassandraTemplate implements Cassand
 
 	/**
 	 * Constructor used for a basic template configuration
-	 * 
+	 *
 	 * @param session must not be {@literal null}.
 	 * @param converter must not be {@literal null}.
 	 */
@@ -85,7 +87,7 @@ public class CassandraAdminTemplate extends CassandraTemplate implements Cassand
 
 	/**
 	 * Create a list of query operations to alter the table for the given entity
-	 * 
+	 *
 	 * @param entityClass
 	 * @param tableName
 	 */
