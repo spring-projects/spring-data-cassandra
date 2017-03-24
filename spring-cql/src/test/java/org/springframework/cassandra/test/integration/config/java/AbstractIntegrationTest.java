455d3f82fc4829d1f4869d643c73962ee0b5e944
Mark Paluch <mpaluch@pivotal.io>
2017-03-24 14:56:34 +0100

    DATACASS-419 - Remove unused dependency declarations.
    Remove el-api and cdi-api from Spring CQL. Remove jsr250-api, el-api, validation-api and hibernate-validator from Spring Data Cassandra.

--- spring-cql/src/test/java/org/springframework/cassandra/test/integration/config/java/AbstractIntegrationTest.java
+++ spring-cql/src/test/java/org/springframework/cassandra/test/integration/config/java/AbstractIntegrationTest.java
@@ -15,26 +15,21 @@
  */
 package org.springframework.cassandra.test.integration.config.java;
 
-import javax.inject.Inject;
-
-import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cassandra.test.integration.AbstractEmbeddedCassandraIntegrationTest;
-import org.springframework.cassandra.test.integration.config.IntegrationTestUtils;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.datastax.driver.core.Session;
 
 /**
+ * Base class for Cassandra integration tests.
+ *
  * @author Matthew T. Adams
+ * @author Mark Paluch
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 public abstract class AbstractIntegrationTest extends AbstractEmbeddedCassandraIntegrationTest {
 
-	@Inject protected Session session;
-
-	@Before
-	public void assertSession() {
-		IntegrationTestUtils.assertSession(session);
-	}
+	@Autowired protected Session session;
 }
