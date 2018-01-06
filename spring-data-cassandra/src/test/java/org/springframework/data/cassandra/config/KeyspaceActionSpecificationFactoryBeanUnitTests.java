2953a55238a8bde11699985fa69321f07739b4b0
Mark Paluch <mpaluch@pivotal.io>
2018-01-06 08:59:42 +0100

    DATACASS-517 - Update copyright years to 2018.

--- spring-data-cassandra/src/test/java/org/springframework/data/cassandra/config/KeyspaceActionSpecificationFactoryBeanUnitTests.java
+++ spring-data-cassandra/src/test/java/org/springframework/data/cassandra/config/KeyspaceActionSpecificationFactoryBeanUnitTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 the original author or authors.
+ * Copyright 2017-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption.Repli
 
 /**
  * Unit tests for {@link KeyspaceActionSpecificationFactoryBean}.
- * 
+ *
  * @author Mark Paluch
  */
 public class KeyspaceActionSpecificationFactoryBeanUnitTests {
