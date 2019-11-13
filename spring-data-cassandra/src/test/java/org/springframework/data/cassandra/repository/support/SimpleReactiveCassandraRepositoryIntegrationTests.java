afebb7cb0698535da363d0af0492017cba00554b
Mark Paluch <mpaluch@pivotal.io>
2019-11-13 11:30:44 +0100

    DATACASS-700 - Retain Sort in paging through repository queries.
    SlicedExecution in repository query methods associates the next Pageable with Sort to propagate the pagination configuration across multiple paging requests.
    Previously, Sort was not considered causing a corrupt paging state.

--- spring-data-cassandra/src/test/java/org/springframework/data/cassandra/repository/support/SimpleReactiveCassandraRepositoryIntegrationTests.java
+++ spring-data-cassandra/src/test/java/org/springframework/data/cassandra/repository/support/SimpleReactiveCassandraRepositoryIntegrationTests.java
@@ -15,13 +15,19 @@
  */
 package org.springframework.data.cassandra.repository.support;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.Arrays;
+import static org.assertj.core.api.Assertions.*;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -34,9 +40,14 @@ import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.core.ReactiveCassandraOperations;
+import org.springframework.data.cassandra.core.query.CassandraPageRequest;
 import org.springframework.data.cassandra.domain.User;
+import org.springframework.data.cassandra.domain.UserToken;
 import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
 import org.springframework.data.cassandra.test.util.AbstractKeyspaceCreatingIntegrationTest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.query.ExtensionAwareQueryMethodEvaluationContextProvider;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -61,8 +72,7 @@ public class SimpleReactiveCassandraRepositoryIntegrationTests extends AbstractK
 		}
 	}
 
-	@Autowired
-	private ReactiveCassandraOperations operations;
+	@Autowired private ReactiveCassandraOperations operations;
 
 	private BeanFactory beanFactory;
 	private ClassLoader classLoader;
@@ -210,6 +220,37 @@ public class SimpleReactiveCassandraRepositoryIntegrationTests extends AbstractK
 	@Test // DATACASS-335
 	public void findAllByEmptyPublisherOfIdShouldReturnResults() {
 		repository.findAllById(Flux.empty()).as(StepVerifier::create).verifyComplete();
+	}
+
+	@Test // DATACASS-700
+	public void findAllWithPagingAndSorting() {
+
+		UserTokenRepostitory repository = factory.getRepository(UserTokenRepostitory.class);
+		repository.deleteAll();
+
+		UUID id = UUID.randomUUID();
+		List<UserToken> users = IntStream.range(0, 100).mapToObj(value -> {
+
+			UserToken token = new UserToken();
+			token.setUserId(id);
+			token.setToken(UUID.randomUUID());
+
+			return token;
+		}).collect(Collectors.toList());
+
+		repository.saveAll(users).then().as(StepVerifier::create).verifyComplete();
+
+		List<UserToken> result = new ArrayList<>();
+		Slice<UserToken> slice = repository.findAllByUserId(id, CassandraPageRequest.first(10, Sort.by("token")))
+				.block(Duration.ofSeconds(10));
+
+		while (!slice.isEmpty() || slice.hasNext()) {
+
+			result.addAll(slice.getContent());
+			slice = repository.findAllByUserId(id, slice.nextPageable()).block(Duration.ofSeconds(10));
+		}
+
+		assertThat(result).hasSize(100);
 	}
 
 	@Test // DATACASS-335
@@ -383,6 +424,10 @@ public class SimpleReactiveCassandraRepositoryIntegrationTests extends AbstractK
 		repository.findById(boyd.getId()).as(StepVerifier::create).expectNextCount(0).verifyComplete();
 	}
 
-	interface UserRepostitory extends ReactiveCassandraRepository<User, String> { }
+	interface UserRepostitory extends ReactiveCassandraRepository<User, String> {}
+
+	interface UserTokenRepostitory extends ReactiveCassandraRepository<UserToken, UUID> {
+		Mono<Slice<UserToken>> findAllByUserId(UUID id, Pageable pageRequest);
+	}
 
 }
