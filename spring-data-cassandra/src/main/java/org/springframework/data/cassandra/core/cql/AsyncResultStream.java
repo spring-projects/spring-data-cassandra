b3a9e1661bba2d422deb3f767a99ef27b15054eb
Mark Paluch <mpaluch@pivotal.io>
2019-12-11 08:56:52 +0100

    DATACASS-656 - Add AsyncResultStream utility.
    AsyncResultStream can forward-stream over a AsyncResultStream by fetching subsequent pages and applying either a Consumer or Collector for non-blocking processing of asynchronously fetched results.

---
+++ spring-data-cassandra/src/main/java/org/springframework/data/cassandra/core/cql/AsyncResultStream.java
@@ -0,0 +1,235 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.cassandra.core.cql;
+
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.stream.Collector;
+
+import org.springframework.util.Assert;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.SettableListenableFuture;
+
+import com.datastax.oss.driver.api.core.cql.AsyncResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+
+/**
+ * Asynchronous supplied sequence of elements supporting sequential operations over a {@link AsyncResultSet a result
+ * set}. An asynchronous stream represents a pipeline of operations to process a {@link AsyncResultSet}.
+ *
+ * @author Mark Paluch
+ * @since 3.0
+ */
+class AsyncResultStream<T> {
+
+	private final AsyncResultSet resultSet;
+
+	private final RowMapper<T> mapper;
+
+	private AsyncResultStream(AsyncResultSet resultSet, RowMapper<T> mapper) {
+		this.resultSet = resultSet;
+		this.mapper = mapper;
+	}
+
+	/**
+	 * Creates a {@link AsyncResultStream} given {@link AsyncResultSet}.
+	 *
+	 * @param resultSet the result set to process.
+	 * @return a new {@link AsyncResultStream} instance.
+	 */
+	static AsyncResultStream<Row> from(AsyncResultSet resultSet) {
+
+		Assert.notNull(resultSet, "AsyncResultSet must not be null");
+
+		return new AsyncResultStream<>(resultSet, (row, rowNum) -> row);
+	}
+
+	/**
+	 * Returns a stream consisting of the results of applying the given function to the elements of this stream.
+	 * <p>
+	 * This is an intermediate operation.
+	 *
+	 * @param <R> The element type of the new stream
+	 * @param mapper a non-interfering, stateless {@link RowMapper}.
+	 */
+	<R> AsyncResultStream<R> map(RowMapper<R> mapper) {
+
+		Assert.notNull(mapper, "RowMapper must not be null");
+
+		return new AsyncResultStream<>(resultSet, mapper);
+	}
+
+	/**
+	 * Performs a mutable reduction operation on the elements of this stream using a {@link Collector} resulting in a
+	 * {@link ListenableFuture}.
+	 * <p>
+	 * This is a terminal operation.
+	 *
+	 * @param <R> the type of the result
+	 * @param <A> the intermediate accumulation type of the {@link Collector}
+	 * @param collector the {@link Collector} describing the reduction
+	 * @return the result of the reduction
+	 */
+	<R, A> ListenableFuture<R> collect(Collector<? super T, A, R> collector) {
+
+		Assert.notNull(collector, "Collector must not be null");
+
+		SettableListenableFuture<R> future = new SettableListenableFuture<>();
+		CollectState<A, R> collectState = new CollectState<>(collector);
+
+		collectState.collectAsync(future, this.resultSet);
+
+		return future;
+	}
+
+	/**
+	 * Performs an action for each element of this stream. This method returns a {@link ListenableFuture} that completes
+	 * without a value ({@code null}) once all elements have been processed.
+	 * <p>
+	 * This is a terminal operation.
+	 * <p>
+	 * If the action accesses shared state, it is responsible for providing the required synchronization.
+	 *
+	 * @param action a non-interfering action to perform on the elements.
+	 */
+	ListenableFuture<Void> forEach(Consumer<T> action) {
+
+		Assert.notNull(action, "Action must not be null");
+
+		SettableListenableFuture<Void> future = new SettableListenableFuture<>();
+		ForwardLoopState loopState = new ForwardLoopState(action);
+
+		loopState.forEachAsync(future, this.resultSet);
+
+		return future;
+	}
+
+	/**
+	 * State object for forward-looping using {@code forEach}.
+	 */
+	class ForwardLoopState {
+
+		private final AtomicInteger rowNumber = new AtomicInteger();
+		private final Consumer<T> consumer;
+
+		ForwardLoopState(Consumer<T> consumer) {
+			this.consumer = consumer;
+		}
+
+		void peekRow(Iterable<Row> rows) {
+			rows.forEach(row -> consumer.accept(mapper.mapRow(row, rowNumber.incrementAndGet())));
+		}
+
+		/**
+		 * Recursive async iteration.
+		 *
+		 * @param target
+		 * @param resultSet
+		 */
+		void forEachAsync(SettableListenableFuture<Void> target, AsyncResultSet resultSet) {
+
+			if (target.isCancelled()) {
+				return;
+			}
+
+			try {
+				peekRow(resultSet.currentPage());
+			} catch (RuntimeException e) {
+				target.setException(e);
+				return;
+			}
+
+			if (!resultSet.hasMorePages()) {
+				target.set(null);
+			} else {
+
+				CompletionStage<AsyncResultSet> nextPage = resultSet.fetchNextPage();
+
+				nextPage.whenComplete((nextResultSet, throwable) -> {
+
+					if (throwable != null) {
+						target.setException(throwable);
+					} else {
+						forEachAsync(target, nextResultSet);
+					}
+				});
+			}
+		}
+	}
+
+	/**
+	 * State object for collecting rows using {@code collect}.
+	 */
+	class CollectState<A, R> {
+
+		private final AtomicInteger rowNumber = new AtomicInteger();
+		private volatile A intermediate;
+		private final Collector<? super T, A, R> collector;
+
+		CollectState(Collector<? super T, A, R> collector) {
+			this.collector = collector;
+			this.intermediate = collector.supplier().get();
+		}
+
+		void collectPage(Iterable<Row> rows) {
+
+			for (Row row : rows) {
+				collector.accumulator().accept(intermediate, mapper.mapRow(row, rowNumber.incrementAndGet()));
+			}
+		}
+
+		R finish() {
+			return collector.finisher().apply(intermediate);
+		}
+
+		/**
+		 * Recursive collection.
+		 *
+		 * @param target
+		 * @param resultSet
+		 */
+		void collectAsync(SettableListenableFuture<R> target, AsyncResultSet resultSet) {
+
+			if (target.isCancelled()) {
+				return;
+			}
+
+			try {
+				collectPage(resultSet.currentPage());
+			} catch (RuntimeException e) {
+				target.setException(e);
+				return;
+			}
+
+			if (!resultSet.hasMorePages()) {
+				target.set(finish());
+			} else {
+
+				CompletionStage<AsyncResultSet> nextPage = resultSet.fetchNextPage();
+
+				nextPage.whenComplete((nextResultSet, throwable) -> {
+
+					if (throwable != null) {
+						target.setException(throwable);
+					} else {
+						collectAsync(target, nextResultSet);
+					}
+				});
+			}
+		}
+	}
+}
---
+++ spring-data-cassandra/src/test/java/org/springframework/data/cassandra/core/cql/AsyncResultStreamUnitTests.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.cassandra.core.cql;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+import org.springframework.util.concurrent.ListenableFuture;
+
+import com.datastax.oss.driver.api.core.cql.AsyncResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+
+/**
+ * Unit tests for {@link AsyncResultStream}.
+ *
+ * @author Mark Paluch
+ */
+public class AsyncResultStreamUnitTests {
+
+	AsyncResultSet first = mock(AsyncResultSet.class);
+	AsyncResultSet last = mock(AsyncResultSet.class);
+	Row row1 = mock(Row.class);
+	Row row2 = mock(Row.class);
+
+	@Test // DATACASS-656
+	public void shouldIterateFirstPage() {
+
+		when(first.currentPage()).thenReturn(Collections.singletonList(row1));
+
+		List<Row> rows = new ArrayList<>();
+
+		AsyncResultStream.from(first).forEach(rows::add);
+
+		assertThat(rows).containsOnly(row1);
+	}
+
+	@Test // DATACASS-656
+	public void shouldIterateMappedFirstPage() {
+
+		when(first.currentPage()).thenReturn(Collections.singletonList(row1));
+
+		List<String> rows = new ArrayList<>();
+
+		AsyncResultStream.from(first).map((row, rowNum) -> "row-" + rowNum).forEach(rows::add);
+
+		assertThat(rows).containsOnly("row-1");
+	}
+
+	@Test // DATACASS-656
+	public void shouldIterateMappedPages() {
+
+		when(first.currentPage()).thenReturn(Collections.singletonList(row1));
+		when(last.currentPage()).thenReturn(Collections.singletonList(row2));
+		when(first.fetchNextPage()).thenReturn(CompletableFuture.completedFuture(last));
+		when(first.hasMorePages()).thenReturn(true);
+
+		List<String> rows = new ArrayList<>();
+
+		AsyncResultStream.from(first).map((row, rowNum) -> "row-" + rowNum).forEach(rows::add);
+
+		assertThat(rows).containsOnly("row-1", "row-2");
+	}
+
+	@Test // DATACASS-656
+	public void shouldPropagateExceptionOnIterate() {
+
+		when(first.currentPage()).thenReturn(Collections.singletonList(row1));
+
+		CompletableFuture<AsyncResultSet> failed = new CompletableFuture<>();
+		failed.completeExceptionally(new RuntimeException("boo"));
+		when(first.fetchNextPage()).thenReturn(failed);
+		when(first.hasMorePages()).thenReturn(true);
+
+		List<String> rows = new ArrayList<>();
+
+		ListenableFuture<Void> completion = AsyncResultStream.from(first).map((row, rowNum) -> "row-" + rowNum)
+				.forEach(rows::add);
+
+		assertThatThrownBy(completion::get).hasRootCauseInstanceOf(RuntimeException.class);
+	}
+
+	@Test // DATACASS-656
+	public void shouldCollectFirstPage() throws ExecutionException, InterruptedException {
+
+		when(first.currentPage()).thenReturn(Collections.singletonList(row1));
+
+		ListenableFuture<List<Row>> collect = AsyncResultStream.from(first).collect(Collectors.toList());
+
+		assertThat(collect.get()).containsOnly(row1);
+	}
+
+	@Test // DATACASS-656
+	public void shouldCollectMappedPages() throws ExecutionException, InterruptedException {
+
+		when(first.currentPage()).thenReturn(Collections.singletonList(row1));
+		when(last.currentPage()).thenReturn(Collections.singletonList(row2));
+		when(first.fetchNextPage()).thenReturn(CompletableFuture.completedFuture(last));
+		when(first.hasMorePages()).thenReturn(true);
+
+		ListenableFuture<List<String>> rows = AsyncResultStream.from(first).map((row, rowNum) -> "row-" + rowNum)
+				.collect(Collectors.toList());
+
+		assertThat(rows.get()).containsOnly("row-1", "row-2");
+	}
+
+	@Test // DATACASS-656
+	public void shouldPropagateExceptionOnCollect() {
+
+		when(first.currentPage()).thenReturn(Collections.singletonList(row1));
+
+		CompletableFuture<AsyncResultSet> failed = new CompletableFuture<>();
+		failed.completeExceptionally(new RuntimeException("boo"));
+		when(first.fetchNextPage()).thenReturn(failed);
+		when(first.hasMorePages()).thenReturn(true);
+
+		ListenableFuture<List<Row>> collect = AsyncResultStream.from(first).collect(Collectors.toList());
+
+		assertThatThrownBy(collect::get).hasRootCauseInstanceOf(RuntimeException.class);
+	}
+}
