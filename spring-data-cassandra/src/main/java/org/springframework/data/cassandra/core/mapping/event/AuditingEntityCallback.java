5a11bd314aa6729d1be8320ca0ad175b62afbd8b
Mark Paluch <mpaluch@vmware.com>
2021-01-07 11:53:23 +0100

    Update copyright year to 2021.
    Closes #1078

--- spring-data-cassandra/src/main/java/org/springframework/data/cassandra/core/mapping/event/AuditingEntityCallback.java
+++ spring-data-cassandra/src/main/java/org/springframework/data/cassandra/core/mapping/event/AuditingEntityCallback.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -46,7 +46,7 @@ public class AuditingEntityCallback implements BeforeConvertCallback<Object>, Or
 		this.auditingHandlerFactory = auditingHandlerFactory;
 	}
 
-	/* 
+	/*
 	 * (non-Javadoc)
 	 * @see org.springframework.data.cassandra.core.mapping.event.BeforeConvertCallback#onBeforeConvert(java.lang.Object, org.springframework.data.cassandra.core.cql.CqlIdentifier)
 	 */
