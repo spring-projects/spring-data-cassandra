90d83a448883bb20484c857570e8a722a0fcda0a
Mark Paluch <mpaluch@vmware.com>
2022-02-14 11:55:22 +0100

    Polishing.
    Use ClassUtils.isAssignableValue(…) instead of ClassUtils.resolvePrimitiveIfNecessary(target).isAssignableFrom(…).
    
    Closes #1218

--- spring-data-cassandra/src/main/java/org/springframework/data/cassandra/core/convert/MappingCassandraConverter.java
+++ spring-data-cassandra/src/main/java/org/springframework/data/cassandra/core/convert/MappingCassandraConverter.java
@@ -924,10 +924,10 @@ public class MappingCassandraConverter extends AbstractCassandraConverter
 	 * @return the converted value.
 	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private Object getPotentiallyConvertedSimpleRead(Object value, @Nullable Class<?> target) {
+	private Object getPotentiallyConvertedSimpleRead(@Nullable Object value, @Nullable Class<?> target) {
 
 		if (value == null || target == null
-				|| ClassUtils.resolvePrimitiveIfNecessary(target).isAssignableFrom(value.getClass())) {
+				|| ClassUtils.isAssignableValue(target, value)) {
 			return value;
 		}
 
