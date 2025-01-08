792885527685a721bbadcb59ed71eb8cdf0237da
Mark Paluch <mark.paluch@broadcom.com>
2025-01-08 10:41:27 +0100

    Remove `ListenableFuture` support.
    Closes #1548

--- spring-data-cassandra/src/main/java/org/springframework/data/cassandra/core/legacy/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Apache Cassandra legacy asynchronous support for easier migration.
- */
-@NonNullApi
-package org.springframework.data.cassandra.core.legacy;
-
-import org.springframework.lang.NonNullApi;
